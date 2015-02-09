2969e8741e378af8069e359994f508aecd6e7435
Omega <omega@alpha.org>
2015-02-09 14:38:16 -0500

    first commit

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import socket, os, random
 if os.path.isfile('quotes.py'): pass
 else: f = open("quotes.py", "w+"); f.write("quotes_list = []"); f.close(); from quotes import quotes_list
-host = "irc.niggerbread.me"; port = 6667; nick = "Kaya"; chan = "#k"; s = socket.socket(); s.connect((host, port)); connected = True; #config
+host = "irc.tm"; port = 6667; nick = "Kaya"; chan = "#tm"; s = socket.socket(); s.connect((host, port)); connected = True; #config
 for packet in ["NICK {0}\r\n".format(nick), "USER {0} {0} {0} :{0}\r\n".format(nick), "JOIN %s" % chan + "\r\n"]: s.send(packet);
 while connected:
 	data = s.recv(1024).strip(); print data;
@@ -15,4 +15,4 @@ while connected:
 		try: del quotes_list[int(data.split("~quotes del ")[1])]; f = open('quotes.py', 'w+'); f.write("quotes_list = {0}".format(quotes_list)); f.close(); s.send("PRIVMSG %s :Quote was deleted!\r\n" % chan); 
 		except Exception as e: s.send("PRIVMSG %s :Quote ID not found!\r\n" % chan);
 	if "~quotes rand" in data: s.send("PRIVMSG %s :%s\r\n" % (chan, random.choice(quotes_list)));
-	if "" in data: s.close(); print "Disconnected from server"
+	if data == "": s.close(); print "Disconnected from server"
